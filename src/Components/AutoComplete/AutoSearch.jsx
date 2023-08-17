bfc4c2d54ca108f14099b0aa252a2bdd97f8f681
ManideepYelugoti <134390562+ManideepYelugoti@users.noreply.github.com>
2023-08-17 12:21:24 +0530

    Add files via upload

---
+++ src/Components/AutoComplete/AutoSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from '@mui/material' ;
+import AutoCompleteComponent from '.';
+
+
+export default function AutoSearch() {
+  return (
+   <Box display={'flex'} >
+        <AutoCompleteComponent/>
+   </Box>
+  )
+}
